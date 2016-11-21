4f004dc39ddd1c6fdc994380d5e39beae26c0357
Juan A. Guzman <juan.guzman07@gmail.com>
2016-11-20 20:03:13 -0500

    Add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/, message: "must be in the format (555) 123-4567"}
+	validates :website, format: {with: /\Ahttps?:\/\/.*\..*\z/, message: "must be in the format http(s)://****.***"}
+	validates :address, format: {with: /\A\d+[^,]+, [^,]+, [A-Z]{2} \d{5}\z/, message: "must be in the format of street address, city, state, zip code"}
 end
